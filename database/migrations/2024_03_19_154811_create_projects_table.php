6942c230c41e3bc0887a040632a5d037a68241f7
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-19 17:21:36 +0100

    created projects table

--- database/migrations/2024_03_19_154811_create_projects_table.php
+++ database/migrations/2024_03_19_154811_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('image')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
