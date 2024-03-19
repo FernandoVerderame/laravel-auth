eb171464bec9341a218d97151e1dca96ffc2223e
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-19 20:01:53 +0100

    added show buttons

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -19,8 +19,18 @@
     </div>
 </div>
 
-<footer class="d-flex justify-content-between align-items-center">
-    <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary mt-4">Back to projects</a>
+<footer class="d-flex justify-content-between align-items-center mt-3">
+    <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary"><i class="fa-solid fa-arrow-rotate-left me-2"></i>Back to projects</a>
+
+    <div class="d-flex justify-content-between gap-2">
+        <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-warning"><i class="fa-solid fa-pencil me-2"></i>Edit</a>
+
+        <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+        <button type="submit" class="btn btn-danger"><i class="fa-regular fa-trash-can me-2"></i>Delete</button>
+        </form>
+    </div>
 </footer>
 
 @endsection
