141d71c0228e99fc4deb92ce52eb5ecac0e1b75b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-19 20:07:57 +0100

    added projects index buttons

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -29,10 +29,17 @@
             <td>{{ $project->created_at }}</td>
             <td>{{ $project->updated_at }}</td>
             <td>
-                <div class="d-flex justify-content-end">
+                <div class="d-flex justify-content-end gap-2">
                     <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-sm btn-primary">
                         <i class="fas fa-eye"></i>
                     </a>
+                    <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-sm btn-warning"><i class="fa-solid fa-pencil"></i></a>
+
+                    <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                    <button type="submit" class="btn btn-sm btn-danger"><i class="fa-regular fa-trash-can"></i></button>
+                    </form>
                 </div>
             </td>
         </tr>
