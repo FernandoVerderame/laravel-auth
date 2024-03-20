fa2bbc1787b4fdbf6624046baa968788529d34a2
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-20 13:45:57 +0100

    added description & image input

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -13,10 +13,28 @@
     <div class="row">
 
         <div class="col-6">
-            <div class="mb-3">
+            <div class="mb-4">
                 <label for="title" class="form-label h3">Title</label>
                 <div class="input-group">
-                    <input type="text" name="title" id="title" class="form-control" placeholder="Project name...">
+                    <input type="text" name="title" id="title" class="form-control" placeholder="Ex.: Laravel DC Comics">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <div class="mb-4">
+                <label for="description" class="form-label h3">Description</label>
+                <div class="input-group">
+                    <textarea type="text" name="description" id="description" class="form-control" placeholder="Project description..." rows="10"></textarea>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <div class="pb-4 border-bottom">
+                <label for="image" class="form-label h3">URL Image</label>
+                <div class="input-group">
+                    <input type="url" name="image" id="image" class="form-control" placeholder="Ex.: https:://..."></input>
                 </div>
             </div>
         </div>
