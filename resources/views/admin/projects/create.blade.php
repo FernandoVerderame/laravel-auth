591a3b08c066361f2aa3ba614e437929aaa8c18b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-20 14:23:30 +0100

    added new project form

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -8,7 +8,8 @@
     <h1>Create new Project</h1>
 </header>
 
-<form action="">
+<form action="{{ route('admin.projects.store') }}" method="POST">
+    @csrf
 
     <div class="row">
 
@@ -16,7 +17,7 @@
             <div class="mb-4">
                 <label for="title" class="form-label h3">Title</label>
                 <div class="input-group">
-                    <input type="text" name="title" id="title" class="form-control" placeholder="Ex.: Laravel DC Comics">
+                    <input type="text" name="title" id="title" class="form-control" placeholder="Ex.: Laravel DC Comics" value="{{ old('title', '') }}">
                 </div>
             </div>
         </div>
@@ -25,7 +26,7 @@
             <div class="mb-4">
                 <label for="description" class="form-label h3">Description</label>
                 <div class="input-group">
-                    <textarea type="text" name="description" id="description" class="form-control" placeholder="Project description..." rows="10"></textarea>
+                    <textarea type="text" name="description" id="description" class="form-control" placeholder="Project description..." rows="10">{{ old('description', '') }}</textarea>
                 </div>
             </div>
         </div>
@@ -34,7 +35,7 @@
             <div class="pb-4 border-bottom">
                 <label for="image" class="form-label h3">URL Image</label>
                 <div class="input-group">
-                    <input type="url" name="image" id="image" class="form-control" placeholder="Ex.: https:://..."></input>
+                    <input type="url" name="image" id="image" class="form-control" placeholder="Ex.: https:://..." value="{{ old('image', '') }}">
                 </div>
             </div>
         </div>
@@ -45,11 +46,11 @@
         <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary"><i class="fa-solid fa-arrow-rotate-left me-2"></i>Back to projects</a>
     
         <div>
-            <button type="reset" class="btn btn-secondary me-2">Refresh input</button>
+            <button type="reset" class="btn btn-primary me-2">Refresh input</button>
             <button type="submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk me-2"></i>Save</button>
         </div>
     </footer>
-    
+
 </form>
 
 
