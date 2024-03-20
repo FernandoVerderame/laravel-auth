0512d5201f08d9b6d974ee93d17fe240d7b81c06
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-20 11:41:20 +0100

    refactoring header index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 
-<header class="d-flex align-items-center justify-content-between">
-    <h1 class="pb-4 mb-4 mt-3 border-bottom">Projects</h1>
+<header class="d-flex align-items-center justify-content-between pb-4 mb-4 mt-3 border-bottom">
+    <h1 class="">Projects</h1>
 
     <form action="{{ route('admin.projects.index') }}" method="GET">
         <div class="input-group">
